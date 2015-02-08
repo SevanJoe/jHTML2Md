e78e933649070ef14632eda27b10414ef5e39320
Sevan Joe <sevancr7@gmail.com>
2015-02-08 21:50:59 +0800

    create markdown files from html files

--- src/main/java/com/runawaybits/html2markdown/Testbed.java
+++ src/main/java/com/runawaybits/html2markdown/Testbed.java
@@ -1,28 +1,28 @@
 package com.runawaybits.html2markdown;
 
+import java.io.File;
 import java.io.IOException;
-import java.net.MalformedURLException;
-import java.net.URL;
+import java.util.List;
 
 public class Testbed {
+    private static final String HTML_FOLDER_PATH = "html";
+    private static final String HTML_SUFFIX = "html";
+    private static final String CHARSET = "Windows-1252";
+    private static final String MARKDOWN_FOLDER_PATH = "markdown";
+    private static final String MARKDOWN_SUFFIX = "md";
+    
     public static void main(String[] args) {
-        URL url;
-        try {
-            url = new URL("http://jsoup.org/");
-            String parsedText = HTML2Md.convert(url, 30000);
-            System.out.println(parsedText);
-
-            // test parse local html file
-//            String pathFile = "test.html";
-//            File f = new File(pathFile);
-//            String parsedFileText = HTML2Md.convertFile(f, "gbk");
-//            System.out.println(parsedFileText);
-
-            System.out.println("done");
-        } catch (MalformedURLException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
+        if (FilesUtil.isExistNotCreate(HTML_FOLDER_PATH)) {
+            List<File> htmlFileList = FilesUtil.getAllFiles(HTML_FOLDER_PATH, HTML_SUFFIX);
+            for (File htmlFile : htmlFileList) {
+                try {
+                    String mdString = HTML2Md.convertFile(htmlFile, CHARSET);
+                    FilesUtil.newFile(MARKDOWN_FOLDER_PATH + "/" + 
+                            FilesUtil.getNameNoSuffix(htmlFile.getName()) + "." + MARKDOWN_SUFFIX, mdString, CHARSET);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
         }
     }
 }
